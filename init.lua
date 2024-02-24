d370e5754cbe193b6dc36ef80095a409507bc57d
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-02-25 01:02:46 +0200

    Update config

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("tokyonight").setup({
+  style = "night",
+  transparent = true,
+})
+
+vim.cmd("colorscheme tokyonight-night")
